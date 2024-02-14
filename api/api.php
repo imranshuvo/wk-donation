53390e7b0636c17596fab7d7b6796e976f0c6540
imranshuvo <imrankhanshuvo@gmail.com>
2024-02-14 12:43:19 +0600

    Added subscription authorization system

--- api/api.php
+++ api/api.php
@@ -8,53 +8,91 @@ function wk_donation_quickpay_payment( $data ){
 	try {
 		$client = new QuickPay(":{$api_key}");
 
-		$invoice_address = array(
+		if($data['donation_type'] == 'onetime'){
 
-		);
+			$payment = $client->request->post('/payments', [
+				'order_id' => uniqid("bbf"),
+				'currency' => 'DKK',
+				'invoice_address[name]' => $data['name'],
+				'invoice_address[phone_number]' => $data['phone'],
+				'invoice_address[email]' => $data['email'],
+				'invoice_address[vat_no]' => $data['cvr']
+			]);
 
-		// $invoice_address['name'] = 'Test name';
-		// $invoice_address['phone_number'] = '+4590890980';
-		// $invoice_address['email'] = 'test@test.com';
-		// $invoice_address['vat_no'] = 'CVR 222'; //this is CVR
+			$status = $payment->httpStatus();
 
-		$payment = $client->request->post('/payments', [
-			'order_id' => uniqid("bbf"),
-			'currency' => 'DKK',
-			'invoice_address[name]' => $data['name'],
-			'invoice_address[phone_number]' => $data['phone'],
-			'invoice_address[email]' => $data['email'],
-			'invoice_address[vat_no]' => $data['cvr']
-		]);
+			if( $status === 201 ){
+				$paymentObject = $payment->asObject();
 
-		$status = $payment->httpStatus();
+				$endpoint = sprintf("/payments/%s/link", $paymentObject->id);
 
-		if( $status === 201 ){
-			$paymentObject = $payment->asObject();
+				$request_data = array(
+					'amount' => $data['amount'] * 100 //amount in cents
+				);
 
-			$endpoint = sprintf("/payments/%s/link", $paymentObject->id);
+				//Enable mobile pay 
+				if($data['payment_method_type'] == 'MobilePay'){
+					
+					$request_data['invoice_address_selection'] = true;
+				}
 
-			$request_data = array(
-				'amount' => $data['amount'] * 100 //amount in cents
-			);
+				//Issue a put request to create payment link
+		        $link = $client->request->put($endpoint, $request_data);
 
-			//Enable mobile pay 
-			if($data['payment_method_type'] == 'MobilePay'){
-				
-				$request_data['invoice_address_selection'] = true;
+		        if ($link->httpStatus() === 200) {
+		            //Get payment link url
+
+		            return array(
+		            	'status' => 200,
+		            	'quickpay_url' => $link->asObject()->url
+		            );
+		        }
 			}
-
-			//Issue a put request to create payment link
-	        $link = $client->request->put($endpoint, $request_data);
-
-	        if ($link->httpStatus() === 200) {
-	            //Get payment link url
-
-	            return array(
-	            	'status' => 200,
-	            	'quickpay_url' => $link->asObject()->url
-	            );
-	        }
 		}
+		
+
+		if($data['donation_type'] == 'recurring'){
+
+			$payment = $client->request->post('/subscriptions', [
+				'order_id' => uniqid("bbf"),
+				'currency' => 'DKK',
+				'invoice_address[name]' => $data['name'],
+				'invoice_address[phone_number]' => $data['phone'],
+				'invoice_address[email]' => $data['email'],
+				'invoice_address[vat_no]' => $data['cvr']
+			]);
+
+			$status = $payment->httpStatus();
+
+			if( $status === 201 ){
+				$paymentObject = $payment->asObject();
+
+				$endpoint = sprintf("/subscriptions/%s/link", $paymentObject->id);
+
+				$request_data = array(
+					'amount' => $data['amount'] * 100 //amount in cents
+				);
+
+				//Enable mobile pay 
+				if($data['payment_method_type'] == 'MobilePay'){
+					$request_data['invoice_address_selection'] = true;
+				}
+
+				//Issue a put request to create payment link
+		        $link = $client->request->put($endpoint, $request_data);
+
+		        if ($link->httpStatus() === 200) {
+		            //Get payment link url
+
+		            return array(
+		            	'status' => 200,
+		            	'quickpay_url' => $link->asObject()->url
+		            );
+		        }
+			}
+		}
+
+		
 
 	} catch ( Exception $e ){
 		//
